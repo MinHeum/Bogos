636b8f0c998c9141807b66ac4205eab905596acd
hobada96 <hobada96@gmail.com>
2019-12-13 06:07:58 +0900

    modified html

--- parser.py
+++ parser.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 
 # from cu import cu_parser
-# from emart import emart_parser
+from emart import emart_parser
 # from gs25 import gs25_parser
 # from seveneleven import seven_parser
-from ministop import ministop_parser
+# from ministop import ministop_parser
 
 
 if __name__ == '__main__':
     # parser들 모아서 실행하는 코드
     # cu_parser()
-    # emart_parser()
+    emart_parser()
     # gs25_parser()
     # seven_parser()
-    ministop_parser()
+    # ministop_parser()
